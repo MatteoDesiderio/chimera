7e50497e9df579f22d6f12d54330b731789668d2
MatteoDesiderio <4matteo.desiderio@gmail.com>
2022-06-23 19:52:56 +0100

    Changed workflow but should be divided in more scripts

--- example/workflow.py
+++ example/workflow.py
@@ -11,26 +11,11 @@ from utils import set_renormalized_fields
 from interfaces.perp.tab import Tab
 from interfaces.perp.thermo_elastic_field import ThermoElasticField
 from interfaces.stag import loader
+from chimera_project import Project
 
-# %% define stagyy model
-model_name = "BS_drhoLM350_z1_PrLM100"
-
-# %% define perplex paths
-perp_path = "/home/matteo/PerpleX_scripts/tab-files/"
-stag_path = "/home/matteo/stagyyRuns/ghostresults/"
-
-# %% define axisem's mesh numpy path
-axi_path = "./axisem_fields/"
-axi_name = "PREM_ISO_2s"
-
-# %% thermodynamic variables and compositional fields
-var_names = ["T", "p_s"]                        # as read by stagyy
-field_names = ["bs", "hz", "prim"]              # as read by stagyy
-# the corresponding perplex projects, order must match
-proj_names = ["bsXu08_1", "hzXu08_1", "primBr100_1"]
-
-# %% tie perplex tab file names to our composition of choice
-proj_names_dict = {k:v for k,v in zip(field_names, proj_names)}
+# load project
+proj_path = "/home/matteo/chimera-projects/Marble-vs-PlumPudding/"
+proj = Project.load(proj_path)
 
 # %% interpolation parameters
 p = 4                     # parameter of shepherds inv dist weight interp
@@ -40,69 +25,67 @@ tree_args = {"leafsize": 10}
 query_args = {"r": 0.08, 
               "return_sorted": True}
 
-# %% Output paths 
-# where are you going to save your thermoelastic fields for each composition?
-thermo_path = "./elastic-fields/"
-# where are you going to save your velocity fields?
-vel_model_path = "./seism_vel-fields/"  
-
 # %% load axisem high resolution grid 
-x = np.load(axi_path + axi_name + "_x.npy")
-y = np.load(axi_path + axi_name + "_y.npy")
+path_modx, path_mody = proj.get_paths()
+x = np.load(path_modx)
+y = np.load(path_mody)
 n = len(x)
-
-# %% initialize variables and fields 
-variables = [Field(v) for v in var_names]
-fields = [Field(v) for v in field_names]
 
 # %% load data from stagyy with stagpy into fields
 # TODO speed up the process by loading coords and fields ath same time
-sdat = stagyydata.StagyyData(stag_path + model_name)
-i_t = -100
+variables = [Field(v) for v in proj.thermo_var_names]
+fields = [Field(v) for v in proj.c_field_names]
 
-for v in variables:
-    v.coords = loader.load_coords(sdat)    
-    v.values = loader.load_field(sdat, v.name, i_t) 
+for model_name in proj.stagyy_model_names:
+    model_path = proj.chimera_project_path + proj.project_name + model_name
+    indices, years = proj.t_indices[model_name], proj.time_span_Gy
+    for i_t, t in zip(indices, years):
+        
+        sdat = stagyydata.StagyyData(proj.stagyy_path + model_name)
 
-for f in fields:
-    f.coords = loader.load_coords(sdat)    
-    f.values = loader.load_field(sdat, f.name, i_t)
+        for v in variables:
+            v.coords = loader.load_coords(sdat)    
+            v.values = loader.load_field(sdat, v.name, i_t) 
 
-# renormalize compositional fields
-set_renormalized_fields(*fields)
+        for f in fields:
+            f.coords = loader.load_coords(sdat)    
+            f.values = loader.load_field(sdat, f.name, i_t)
 
-# %% Initialize Velocity Model
-v_model = VelocityModel(model_name, field_names)
+        # renormalize compositional fields
+        set_renormalized_fields(*fields)
 
-# %% interpolating stagyy fields on larger axisem grid
-# TODO load the T, P, c directly into the class VelocityModel (class attr?)
-v_model.T = variables[0].interpolate(x, y, p, tree_args, query_args)
-v_model.P = variables[1].interpolate(x, y, p, tree_args, query_args)
+        v_model = VelocityModel(model_name, proj.c_field_names)
+        
+        snap_path = model_path + "{}/".format(i_t)
 
-for i, f in enumerate(fields):
-    v_model.C[i] = f.interpolate(x, y, p, tree_args, query_args)
+        # interpolating stagyy fields on larger axisem grid
+        v_model.T = variables[0].interpolate(x, y, p, tree_args, query_args)
+        v_model.P = variables[1].interpolate(x, y, p, tree_args, query_args)
+        
+        for i, f in enumerate(fields):
+            v_model.C[i] = f.interpolate(x, y, p, tree_args, query_args)
     
 # %% saving moduli and rho from PerpleX table in an edible format (npy)
 # TODO speed up with numba (tab load especially)
-print("Saving the moduli for each composition")
-tabs = []
-thermo_fields = []
-T, P = v_model.T, v_model.P                            # thermodyamic variables     
-for i, f in enumerate(field_names):
-    inpfl = perp_path + proj_names_dict[f] + ".tab"    # where are the tables
-    tab = Tab(inpfl)                                   # initialize table
-    tab.load()                                         # load values
-    tab.remove_nans()                                  # sanitize
-    thermo_field = ThermoElasticField(tab, f)  # initialize thermoelastic field
-    thermo_field.extract(T, P, model_name)     # create/save thermoel field
-    thermo_field.save(thermo_path + model_name)
-    tabs.append(tab)
-    thermo_fields.append(thermo_field)
 
-# %%
-v_model.average(*v_model.load_moduli(thermo_path, proj_names_dict))
-v_model.compute_velocities()
-v_model.save(vel_model_path)
+        tabs = []
+        thermo_fields = []
+        T, P = v_model.T, v_model.P                            
+        for i, f in enumerate(proj.c_field_names):
+            inpfl = proj.perplex_path + proj.proj_names_dict[f] + ".tab" 
+            tab = Tab(inpfl)                                   
+            tab.load()                                        
+            tab.remove_nans()                                  
+            thermo_field = ThermoElasticField(tab, f)  
+            thermo_field.extract(T, P, model_name)     
+            thermo_field.save(snap_path + proj.elastic_path)
+            tabs.append(tab)
+            thermo_fields.append(thermo_field)
+
+        v_model.average(*v_model.load_moduli(snap_path + proj.elastic_path, 
+                                             proj.proj_names_dict))
+        v_model.compute_velocities()
+        v_model.save(snap_path + proj.vel_model_path)
 
 # %%
 plt.figure()
