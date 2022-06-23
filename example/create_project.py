c570605636f3efb63ffb5d73e96da930b6576529
MatteoDesiderio <4matteo.desiderio@gmail.com>
2022-06-23 19:50:15 +0100

    added script to create a project

---
+++ example/create_project.py
@@ -0,0 +1,36 @@
+""" Here I show how to create a new project"""
+
+import sys
+sys.path.append('..')
+
+from chimera_project import Project
+
+# %% 
+proj = Project()
+# directory of project
+proj.chimera_project_path = "/home/matteo/chimera-projects/"
+
+# define stagyy models location and model names
+proj.stagyy_path = "/home/matteo/stagyyRuns/ghostresults/"
+proj.stagyy_model_names = ["BS_drhoLM350_z1_PrLM100", 
+                           "BS_drhoLM230_z1_PrLM100"] 
+
+# define perplex paths, where tab files are located
+proj.perplex_path = "/home/matteo/PerpleX_scripts/tab-files/"
+
+# % define axisem's mesh numpy path
+proj.bg_model = "PREM_ISO_2s"
+
+# thermodynamic variables and compositional fields
+proj.thermo_var_names = ["T", "p_s"]            # as read by stagyy
+proj.c_field_names = ["bs", "hz", "prim"]       # as read by stagyy
+# the corresponding perplex projects, order must match with above
+proj.perplex_proj_names = ["bsXu08_1", "hzXu08_1", "primBr100_1"]
+
+# define time span in billion years
+proj.time_span_Gy = [4.5]
+
+
+# finally create project with the name
+proj_name = "Marble-vs-PlumPudding"
+proj.new(proj_name)
