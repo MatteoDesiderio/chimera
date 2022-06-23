3a648ea5f8abb2833498f1a380573dbc70dda50c
MatteoDesiderio <4matteo.desiderio@gmail.com>
2022-06-23 15:18:52 +0100

    Created class Project

---
+++ example/chimera_project.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jun 23 15:14:07 2022
+
+@author: matteo
+"""
+class Project:
+    def __init__(self):
+        self.chimera_project_path = "" # path where you want to save project
+        self.project_name = "" # path containing your project
+        self.bg_model = "" # name of your axisem bg model
+        self.perplex_path = "" # path to tab files
+        self.stagyy_path = ""
+        self.axi_path = ""
+        self.stagyy_model_names = []
+        
