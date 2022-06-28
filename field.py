3bbaf1d6b5e9aa6b82edfc8057b34a2e83f980c9
MatteoDesiderio <4matteo.desiderio@gmail.com>
2022-06-28 17:59:30 +0100

    Introduce possibility to perform simple bilinear interpolation. To be fixed

--- field.py
+++ field.py
@@ -25,25 +25,35 @@ def _inverse_dist_weighting(indices, new, old, z, p):
 
     return znew
 
-@nb.njit(parallel=True)
-def _bilinear_weighting(indices, new, old, z, p):
-    znew = np.empty(new.shape[0], dtype=new.dtype)
-    for i in nb.prange(new.shape[0]):
-        num = 0.0
-        den = 0.0
-        for j in indices[i]:
-            d = np.sqrt((new[i, 0] - old[j, 0]) ** 2 +
-                        (new[i, 1] - old[j, 1]) ** 2)
-            if d > 0.0:
-                num = num + np.power(d, - p) * z[j]
-                den = den + np.power(d, - p)
-            else:
-                num = z[j]
-                den = 1.0
-                break
-        # maybe there's a better way to handle this case
-        znew[i] = num / den if (num > 0 and den > 0) else 0
 
+@nb.njit(parallel=True)
+def _bilinear_weighting(indices, new, old, z):    
+    znew = np.empty(new.shape[0], dtype=new.dtype)
+    # for every point in the finer grid
+    for i in nb.prange(new.shape[0]):
+        x, y = new[i] # get coordinates of said point
+        # initialize rectangel around said point
+        corners = np.empty((4, 2)) 
+        v_corners = np.empty((4, 2), dtype=z.dtype)
+        # for each of the four closest corners
+        for u, j in enumerate(indices[i]):
+            corners[u] = old[j] # get their coordinates
+            v_corners[u] = z[j] # get the value of the function at corners
+        
+        # I only need x, y coordinates
+        x1, x2 = np.min(corners[:, 0]), np.max(corners[:, 0])
+        y1, y2 = np.min(corners[:, 1]), np.max(corners[:, 1])
+        
+        # see wikipedia
+        vec = np.array([1, x, y, x*y])
+        mat = np.array([[x2*y2, -y2, -x2, 1.0], 
+                       [-x2*y1, y1,  x2, -1.0], 
+                       [-x1*y2, y2,  x1, -1.0], 
+                       [x1*y1, -y1, -x1,  1.0]])
+        
+        # set the value at the point of the finer grid
+        znew[i] = v_corners @ mat @ vec / (x2 - x1) / (y2 - y1)
+        
     return znew
 
 class Field:
@@ -153,11 +163,31 @@ class Field:
         x, y = self.to_cartesian()
         old = np.c_[x, y]
         z = self.values.flatten()
-        kdtree = KDTree(old, **kdtree_kwargs)
         new = np.c_[xnew, ynew]
-        inds = kdtree.query_radius(new, **query_kwargs)
         
-        return _inverse_dist_weighting(inds, new, old, z, p)
+        kdtree = KDTree(old, **kdtree_kwargs)
+        
+        # if the radius of search, we look at just the 4 closest neighbours
+        # p does not matter at that point, query_kwargs["k"] is set to 4
+        do_bilinear = False
+        if "r" in query_kwargs.keys():
+            if query_kwargs["r"] == 0.0:
+                do_bilinear = True
+                query_kwargs.pop("r")
+            else:
+                do_bilinear = False
+        else:
+            do_bilinear = True
+        
+        if do_bilinear:
+            query_kwargs["k"] = 4
+            _, inds = kdtree.query(new, **query_kwargs)
+            result = _bilinear_weighting(inds, new, old, z)
+        else:
+            inds = kdtree.query_radius(new, **query_kwargs)
+            result = _inverse_dist_weighting(inds, new, old, z, p)
+        
+        return result
         
     def bil_interpolate(self, xnew, ynew, p=2,
                     kdtree_kwargs={"leafsize": 10},
