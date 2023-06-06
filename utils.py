Fix average method

utils.py
@@ -4,9 +4,8 @@ Several misc utilities
 import numpy as np
 import numba as nb
 from scipy.interpolate import interp1d
-from scipy.signal.windows import tukey
+from scipy.signal.windows import tukey, bartlett, triang
 from scipy.stats import multivariate_normal
-from skimage import transform
 
 import matplotlib.pyplot as plt
 plt.ion()
@@ -61,9 +60,9 @@ class Downsampler:
         else:
             self.ynew = ynew
                     
-    def downsample(self, z, avg=True):
+    def downsample(self, z, method="avg"):
         
-        if avg:
+        if method=="gauss":
             # 1) resample original mesh (dr is not constant)
             dx = np.diff(np.abs(self.x)).min()
             dy = np.diff(np.abs(self.y)).min()
@@ -96,7 +95,7 @@ class Downsampler:
             return None, znew.T
             
             
-        else:
+        elif method=="fourier":
             # 1) resample original mesh (dr is not constant)
             dx = np.diff(np.abs(self.x)).min()
             dy = np.diff(np.abs(self.y)).min()
@@ -144,6 +143,45 @@ class Downsampler:
             
             xx, yy = to_cartesian(x_int, self.y)
             coords = np.c_[xx, yy]
+            
             return coords, filt_z_int
         
+        else:
+            # 1) resample original mesh (dr is not constant)
+            dx = np.abs(np.diff(self.x)).min()
+            dy = np.abs(np.diff(self.y)).min()
+            shape = self.y.size, self.x.size
+            f = interp1d(self.x, z.reshape(shape))
+            x_int = np.arange(self.x.min(), self.x.max() + dx, dx)
+            x_int[-1] = 1
+            z_int = f(x_int)
+            ny, nx = z_int.shape
+            
+            xx, yy = np.meshgrid(x_int, self.y)
+            pos = np.dstack((xx, yy))
+            
+            znew = np.zeros([self.xnew.size, self.ynew.size])
+            xdiffs = np.abs(np.diff(self.xnew))
+            xdiffs = np.r_[xdiffs, xdiffs[-1]] / 2
+            ydiffs = np.abs(np.diff(self.ynew))
+            ydiffs = np.r_[ydiffs, ydiffs[-1]] / 2
+
+            for ix, mux in enumerate(self.xnew):
+                sx = xdiffs[ix]
+                for iy, muy in enumerate(self.ynew):
+                    sy = ydiffs[iy]
+                    weights = np.zeros_like(z_int)
+                    squarex = (xx >= mux - sx) & (xx <= mux + sx)
+                    squarey = (yy >= muy - sy) & (yy <= muy + sy)
+                    square = squarex & squarey
+                    weights[square] = 1.0
+                    if method == "triang":
+                        mx = len(squarex[0, :][squarex[0, :]])
+                        my = len(squarey[:, 0][squarey[:, 0]])
+                        triangx = bartlett(mx) 
+                        triangy = triang(my)
+                        pyramid = triangy[:, np.newaxis] * triangx
+                        weights[square] *= pyramid.flatten()
+                    znew[ix, iy] = np.sum(weights * z_int) / np.sum(weights)
+            return None, znew.T
     