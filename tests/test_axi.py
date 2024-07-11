627a65110fdc8c9d58b0eff02ca25d4d513f249e
Matteo <4matteo.desiderio@gmail.com>
2024-07-11 19:47:51 +0100

    Fix test_axi.

--- tests/test_axi.py
+++ tests/test_axi.py
@@ -3,14 +3,14 @@ from chimera.interfaces.axi.mesh_importer import MeshImporter
 from os.path import exists
 
 
-
-def test_importer(self):
-    axisem_path = "./axisem_test_path/"
-    importer = MeshImporter(axisem_path, mesh_path="PREM_ISO_10s")
+def test_importer():
+    axisem_path = "examples/axisem_test_path/"
+    importer = MeshImporter(axisem_path,
+                            mesh_path="PREM_ISO_LIGHT_10s")
 
     x, y = importer.convert_to_numpy("./")
     assert isinstance(x, np.ndarray)
     assert isinstance(y, np.ndarray)
     # assert x.shape[-1] == 2
-    assert exists("./" + self.importer.mesh_name + "_x.npy")
-    assert exists("./" + self.importer.mesh_name + "_y.npy")
+    assert exists("./" + importer.mesh_name + "_x.npy")
+    assert exists("./" + importer.mesh_name + "_y.npy")
