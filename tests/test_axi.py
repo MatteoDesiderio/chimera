43569b4718fbfa830b80e50731fff21763287364
Matteo <4matteo.desiderio@gmail.com>
2024-07-11 19:39:51 +0100

    Fix test_axi

--- tests/test_axi.py
+++ tests/test_axi.py
@@ -1,25 +1,16 @@
-"""
-TODO translate to pytest
-"""
-import unittest
 import numpy as np
-from mesh_importer import MeshImporter
+from chimera.interfaces.axi.mesh_importer import MeshImporter
 from os.path import exists
 
 
-class TestFields(unittest.TestCase):
-    def setUp(self):
-        axisem_path = "./axisem_test_path/"
-        self.importer = MeshImporter(axisem_path, mesh_path="PREM_ISO_10s")
 
-    def test_importer(self):
-        x, y = self.importer.convert_to_numpy("./")
-        assert isinstance(x, np.ndarray)
-        assert isinstance(y, np.ndarray)
-        # assert x.shape[-1] == 2
-        assert exists("./" + self.importer.mesh_name + "_x.npy")
-        assert exists("./" + self.importer.mesh_name + "_y.npy")
+def test_importer(self):
+    axisem_path = "./axisem_test_path/"
+    importer = MeshImporter(axisem_path, mesh_path="PREM_ISO_10s")
 
-
-if __name__ == "__main__":
-    unittest.main()
+    x, y = importer.convert_to_numpy("./")
+    assert isinstance(x, np.ndarray)
+    assert isinstance(y, np.ndarray)
+    # assert x.shape[-1] == 2
+    assert exists("./" + self.importer.mesh_name + "_x.npy")
+    assert exists("./" + self.importer.mesh_name + "_y.npy")
