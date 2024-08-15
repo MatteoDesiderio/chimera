7e599b3e367bbfcb45f3939bd55f1a68aca98273
Matteo <4matteo.desiderio@gmail.com>
2024-08-15 18:18:43 +0100

    Remove tests

---
+++ tests/test_axi.py
@@ -0,0 +1,18 @@
+"""import numpy as np
+from chimera.interfaces.axi.mesh_importer import MeshImporter
+from os.path import exists, dirname
+import pytest
+
+def test_importer(tmp_path):
+    tests_path = dirname(__file__)
+    axisem_path = f"{tests_path}/examples/inputData/axisemFolder"
+    importer = MeshImporter(axisem_path,
+                            mesh_path="PREM_ISO_LIGHT_10s")
+
+    x, y = importer.convert_to_numpy("./")
+    assert isinstance(x, np.ndarray)
+    assert isinstance(y, np.ndarray)
+    # assert x.shape[-1] == 2
+    assert exists("./" + importer.mesh_name + "_x.npy")
+    assert exists("./" + importer.mesh_name + "_y.npy")
+"""
