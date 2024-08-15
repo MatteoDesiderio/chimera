5a23e6ba057fe130635e3139d91044219692de24
Matteo <4matteo.desiderio@gmail.com>
2024-08-15 18:57:55 +0100

    Add test configuration script, fix test_axi

--- tests/test_axi.py
+++ tests/test_axi.py
@@ -1,6 +1,5 @@
 """import numpy as np
 from chimera.interfaces.axi.mesh_importer import MeshImporter
-from os.path import exists, dirname
 import pytest
 
 def test_importer(tmp_path):
@@ -16,3 +15,22 @@ def test_importer(tmp_path):
     assert exists("./" + importer.mesh_name + "_x.npy")
     assert exists("./" + importer.mesh_name + "_y.npy")
 """
+import pytest
+
+from os.path import exists
+import numpy as np
+
+from chimera.interfaces.axi.mesh_importer import MeshImporter
+
+def test_importer(tmp_path, example_dir):
+    axisem_path = f"{example_dir}/inputData/axisemFolder"
+    importer = MeshImporter(axisem_path,
+                            mesh_path="PREM_ISO_LIGHT_10s")
+    
+    temporary_path = tmp_path.as_posix()
+    x, y = importer.convert_to_numpy(temporary_path)
+    assert isinstance(x, np.ndarray)
+    assert isinstance(y, np.ndarray)
+    # assert x.shape[-1] == 2
+    assert exists(f"{temporary_path}/{importer.mesh_name}_x.npy")
+    assert exists(f"{temporary_path}/{importer.mesh_name}_y.npy")
